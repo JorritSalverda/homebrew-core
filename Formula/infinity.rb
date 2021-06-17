cf8dd9584ade22e7e2813d59740959026c3bb15e
Jorrit Salverda <jsalverda@travix.com>
2021-06-17 12:14:35 +0200

    update sha256 for infinity

--- Formula/infinity.rb
+++ Formula/infinity.rb
@@ -2,7 +2,7 @@ class Infinity < Formula
     desc "Infinity is a CLI to easily build your applications using a pipeline as code"
     homepage "https://github.com/JorritSalverda/infinity"
     url "https://github.com/JorritSalverda/infinity/releases/download/v0.1.0/infinity-v0.1.0-darwin-amd64.zip"
-    sha256 "458447ad4794c2939fa9913621aba660183b7dba57253f3a1378b05c7f1f84a1"
+    sha256 "f9e0b74928a05e8380116d835b2021d54809f2d98ae856419a50845ab65d88bc"
     version "0.1.0"
 
   def install

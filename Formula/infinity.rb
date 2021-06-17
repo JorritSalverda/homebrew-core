968a8c6bf4ba90488e4609bd236889a84f8c593e
Jorrit Salverda <jsalverda@travix.com>
2021-06-17 11:59:18 +0200

    add homepage to formula

--- Formula/infinity.rb
+++ Formula/infinity.rb
@@ -1,5 +1,6 @@
 class Infinity < Formula
     desc "Infinity is a CLI to easily build your applications using a pipeline as code"
+    homepage "https://github.com/JorritSalverda/infinity"
     url "https://github.com/JorritSalverda/infinity/releases/download/v0.1.0/infinity-v0.1.0-darwin-amd64.zip"
     sha256 "458447ad4794c2939fa9913621aba660183b7dba57253f3a1378b05c7f1f84a1"
     version "0.1.0"

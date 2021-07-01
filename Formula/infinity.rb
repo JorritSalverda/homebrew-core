da2d5e9b15c3e54c7c7017a7d40464e0b920cdcc
Jorrit Salverda <jorrit.salverda@gmail.com>
2021-07-01 05:45:45 +0200

    infinity v0.1.24

--- Formula/infinity.rb
+++ Formula/infinity.rb
@@ -1,12 +1,12 @@
 class Infinity < Formula
     desc "Infinity is a CLI to easily build your applications using a pipeline as code"
     homepage "https://github.com/JorritSalverda/infinity"
-    url "https://github.com/JorritSalverda/infinity/releases/download/v0.1.23/infinity-v0.1.23-darwin-amd64.zip"
-    sha256 "87b2f1d536b9fb066637ff2bb3b365806b93a779c79645cb2c5c0f8f91d87c9b"
-    version "0.1.23"
+    url "https://github.com/JorritSalverda/infinity/releases/download/v0.1.24/infinity-v0.1.24-darwin-amd64.zip"
+    sha256 "af8d64b09738f7b312545936bb389589dc4549f7dc56d152f05201dc107b3e30"
+    version "0.1.24"
 
   def install
-    bin.install "infinity-v0.1.23-darwin-amd64" => "infinity"
+    bin.install "infinity-v0.1.24-darwin-amd64" => "infinity"
   end
 
   test do
